86eafdb07636ecddb29143646b7f293963b5e8c2
cpoakley1432 <cpoakley1432@gmail.com>
2015-11-10 12:18:25 -0500

    initial import

---
+++ src/main/java/com/theironyard/Beer.java
@@ -0,0 +1,21 @@
+package com.theironyard;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+/**
+ * Created by cameronoakley on 11/10/15.
+ */
+@Entity
+public class Beer {
+    @Id
+    @GeneratedValue
+    Integer id;
+
+    String name;
+    String type;
+
+
+}
